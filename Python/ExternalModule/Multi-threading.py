c2d98785e488e6c99430a422e3afacb37e4736c1
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-06-13 10:01:40 +0530

    update the multi-threading file

--- Python/ExternalModule/Multi-threading.py
+++ Python/ExternalModule/Multi-threading.py
@@ -1,20 +1,27 @@
-# Ex :- import the github data of 100 user
+# Ex :- import the github data of 100 user with the help of multithreading 
+# If not using the multithreading we need to wait for all the process to complete
 
 import threading
 import time
 
 def worker(num):
     print(f"Thread {num}: Starting")
-    time.sleep(2) # Simulate some work dealy of 2 sec
+    time.sleep(2)                                           # Simulate some work dealy of 2 sec
     print(f"Thread {num}: Finishing")
 
 
 threads = []
 for i in range(3):
-    thread = threading.Thread(target=worker, args=(i,))
-    threads.append(thread)
+    thread = threading.Thread(target=worker, args=(i,))       # make the tread for parllel execution
+    threads.append(thread)                                   # append the making thread in list
     thread.start()
+
 for thread in threads:
-    thread.join() # Wait for all threads to finish
+    thread.join()   
+    
+    # Wait for all threads to finish if not using join it will not wait for 2 sec it will execute other things
     
 print("All threads completed.")
+
+
+# Go and explore multi-process module it run process instead of thread
